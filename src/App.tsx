27585eec1a5cb70d705675b427501e3b7b6e1f2e
ma9pie <ma9pie.dev@gmail.com>
2024-05-18 05:12:56 +0900

    fix : mobx example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observer } from "mobx-react";
+import { makeAutoObservable } from "mobx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter {
+  count = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increment = () => {
+    this.count++;
+  };
 }
 
-export default App;
+const counter = new Counter();
+
+const App = () => {
+  const { count, increment } = counter;
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+};
+
+export default observer(App);
